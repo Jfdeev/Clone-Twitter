d45308e7038e6d70fc043f7ad99280cee3c17f2f
Jfdeev <jfrsoares03@gmail.com>
2024-12-04 12:40:06 -0300

    finish to connect to the db

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const app = express();
 
 const db = require('./models');
 
+  
 db.sequelize.sync().then(() => {
     app.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
 });
+
 
 
