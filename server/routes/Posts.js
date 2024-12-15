a54a2c592e8d30c9ab5c7baea6b17d335e182ad6
Jfdeev <jfrsoares03@gmail.com>
2024-12-15 16:29:42 -0300

    integrando front com back end

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { Posts } = require('../models');
 
-router.get('/', (req, res) => {
-    res.json('Posts');
+router.use(express.json());
+
+//solicitar posts
+router.get('/', async (req, res) => {
+    const listOfPosts =  await Posts.findAll()
+    res.json(listOfPosts);
 });
 
-router.get('/specific', (req, res) => {
+//criar um post
+router.post('/', async (req, res) => {
     const post = req.body;
+    await Posts.create(post)
+    res.json(result);
+    
 });
 
 module.exports = router;
